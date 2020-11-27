8318de7aa74408f01d0323f22c2a5853211452ac
krion <a920325081@qq.com>
2020-11-27 10:59:32 +0800

    Initial commit

---
+++ src/main/java/com/jlq/MainApplication.java
@@ -0,0 +1,18 @@
+package com.jlq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：jlq
+ * @date ：Created in 2020/11/27 10:57
+ */
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
+
+
